936c5c9ce4962a8bd85cb1c9de8b3cc223a2ed6d
jubairk <jubairskv@gmail.com>
2025-01-27 01:25:58 +0530

    inital commit

--- example/src/App.jsx
+++ example/src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Button, View, Text } from 'react-native';
-import mylibrary from 'dummy-text-library';
+import mylibrary from 'react-native-my-library';
 
 const App = () => {
   const [dummyText, setDummyText] = useState('');
