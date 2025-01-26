032f5fda9168dcacd9e108c6fc007a928e9c5342
jubairk <jubairskv@gmail.com>
2025-01-27 01:01:54 +0530

    inital commit

--- android/src/main/java/com/mylibrary/MyLibraryModule.kt
+++ android/src/main/java/com/mylibrary/MyLibraryModule.kt
@@ -1,25 +1,17 @@
-package com.mylibrary
-
 import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Promise
 
-class MyLibraryModule(reactContext: ReactApplicationContext) :
-  ReactContextBaseJavaModule(reactContext) {
+class MyLibraryModule(reactContext: ReactApplicationContext) : ReactContextBaseJavaModule(reactContext) {
 
-  override fun getName(): String {
-    return NAME
-  }
+    override fun getName(): String {
+        return "MyLibrary"
+    }
 
-  // Example method
-  // See https://reactnative.dev/docs/native-modules-android
-  @ReactMethod
-  fun multiply(a: Double, b: Double, promise: Promise) {
-    promise.resolve(a * b)
-  }
-
-  companion object {
-    const val NAME = "MyLibrary"
-  }
+    @ReactMethod
+    fun getDummyText(promise: Promise) {
+        val dummyText = "This is some dummy text from the native module!"
+        promise.resolve(dummyText)
+    }
 }
